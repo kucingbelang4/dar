ce96a8d00bd44c48914d64a64dbd05037bc14320
kucingbelang4 <imam.tauhid.dar@gmail.com>
2013-10-09 20:49:38 +0700

    change index js create object

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Config = require('./config'),
 	Server = require('./server');
 
-var dar = Object.create(Server, { config('192.168.244.199', '80') });
+var dar = Object.create(Server);
 
 dar.start(); 
